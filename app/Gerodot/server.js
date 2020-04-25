bb12e7f45aa1b85d6c4f237c335f4820a40f22c7
Denys <denys.vriukalo@gmail.com>
2020-04-25 16:47:27 +0300

    routes, models and controllers

--- app/Gerodot/server.js
+++ app/Gerodot/server.js
@@ -3,6 +3,13 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const app = express();
+app.use(function (req, res, next) {
+    console.log('Time:', Date.now());
+    next();
+  });
+
+/*const db = require("./app/models");
+db.sequelize.sync();*/
 
 var corsOptions = {
   origin: "http://localhost:8081"
@@ -10,19 +17,16 @@ var corsOptions = {
 
 app.use(cors(corsOptions));
 
-// parse requests of content-type - application/json
 app.use(bodyParser.json());
 
-// parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Hello World!" });
+    res.end('Hello World!')
 });
 
-// set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+    console.log(`Server is running on port ${PORT}.`);
 });
