45815beb5febf9a6aa78f7f6e44a504a0fe7dd6f
Yehor Lebed <lebed.egor01@gmail.com>
2020-05-04 11:51:59 +0300

    added user info dropdown + fixed font

--- fe/src/components/user-block/user-block.jsx
+++ fe/src/components/user-block/user-block.jsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { UserInfoDropdown } from '../';
 import './user-block.css';
 
-const UserBlock = ({ avatar, name, children }) => (
-  <div className="user-block">
-    <div className="user-info">
-      <div className="user-info__avatar">{avatar}</div>
-      <div className="user-info__name">{name}</div>
+const UserBlock = ({ avatar, name, children }) => {
+  const [isInfoHidden, setInfoHidden] = useState(true);
+  return (
+    <div className={`user-block ${!isInfoHidden && 'info-hidden'}`}>
+      <div className="user-block__inner">
+        <div
+          className="user-info"
+          onClick={() => setInfoHidden(!isInfoHidden)}
+        >
+          <div className="user-info__avatar">{avatar}</div>
+          <div className="user-info__name">{name}</div>
+        </div>
+        <UserInfoDropdown isHidden={isInfoHidden} />
+        {children}
+      </div>
     </div>
-    {children}
-  </div>
-);
+  )
+}
 
 export default UserBlock;
