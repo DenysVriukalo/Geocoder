1ff7aeb4df4c1fd0f9608740e4cd24302171ca04
Sergej Gritsay <sergej.gritsay@gmail.com>
2020-05-12 12:54:17 +0300

    chenged google map

--- fe/src/components/google-map/google-map.jsx
+++ fe/src/components/google-map/google-map.jsx
@@ -1,18 +1,64 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState, useRef, useCallback } from 'react';
 import { connect } from 'react-redux';
-
 import { array, func } from 'prop-types';
 import { functions, isEqual, omit } from 'lodash'
 
 import API_KEY from '../../utils/api-key';
 import './google-map.css';
 
-function GoogleMap({ options, onMount, places }) {
+function GoogleMap({ options, places }) {
   const ref = useRef();
   const [map, setMap] = useState();
+  const prevMarkersRef = useRef([]);
+  const [markers, setMarkers] = useState([]);
+  //const [counter, setCounter] = useState(1);
+let counter = 1
+   // Adds a marker to the map and push to the array.
+  const addMarkers = (places) => {
+    
+    places.forEach((place) => {
+      let marker = new window.google.maps.Marker({
+        map,
+        position: place.coords,
+        label: `${counter}`,
+        title: place.title,
+      });
+      //setCounter(counter+1)
+      counter++
+      marker.addListener(`click`, () => {
+        window.location.href = place.url
+      });
+      //prevMarkersRef.current.push(marker);
+    
+    })
+  };
+
+  // Removes the markers from the map
+  const clearMarkers = (markers) => {
+    if (markers.length) {
+      for (let m of markers) {
+        m.setMap(null);
+      }
+    }
+  }
+
+  const cb = useCallback(function clearMarkers() {
+    for(let i = 0; i < markers.length; i++) {
+      markers[i].setMap(null);
+    }
+  }, [markers.length]);
+
 
   useEffect(() => {
-    const onLoad = () => setMap(new window.google.maps.Map(ref.current, options))
+    const onLoad = () => {
+      setMap(new window.google.maps.Map(ref.current, options));
+
+      // clearMarkers(prevMarkersRef.current); //clear prev markers
+
+      // addMarkers(places);
+
+      
+    }
     if (!window.google) {
       const script = document.createElement(`script`);
       script.src = process.env.REACT_APP_GOOGLE_MAPS_API_KEY
@@ -24,15 +70,46 @@ function GoogleMap({ options, onMount, places }) {
     } else onLoad();
   }, [options])
 
-  if (map && typeof onMount === `function`) onMount(map, places)
 
+  
+
+// useEffect(() => {
+//     cb();
+//     if (places.length) {
+//       const googleMarkers = [];
+//       for (let i = 0; i < places.length; i++) {
+//         const marker = `marker #${i}`;
+//         googleMarkers.push(marker);
+//       }
+//       setMarkers(googleMarkers);
+//     }
+//   }, [places, cb]);
+  
+  // useEffect(() => {
+  //   const onLoad = () => {
+  //     clearMarkers(prevMarkersRef.current); //clear prev markers
+  //     setMap(new window.google.maps.Map(ref.current, options))
+  //   } 
+  //   if (!window.google) {
+  //     const script = document.createElement(`script`);
+  //     script.src = process.env.REACT_APP_GOOGLE_MAPS_API_KEY
+  //       ? `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`
+  //       : `https://maps.googleapis.com/maps/api/js?key=${API_KEY.REACT_APP_GOOGLE_MAPS_API_KEY}`
+  //     document.head.append(script);
+  //     script.addEventListener(`load`, onLoad);
+  //     return () => script.removeEventListener(`load`, onLoad);
+  //   } else onLoad();
+  // }, [options])
+
+  if (map)  addMarkers(places);
+  
   return (
     <div className="map-container">
-      <div
-        style={{ height: `100%`, borderRadius: `20px`, overflow: `hidden`, border: `1px solid var(--yellow)` }}
+      <div style={{ height: `100%`, borderRadius: `20px`, overflow: `hidden`, border: `1px solid var(--yellow)` }}
         {...{ ref }}
       />
     </div>
+    
   )
 }
 
@@ -60,5 +137,6 @@ GoogleMap.defaultProps = {
   options: {
     center: { lat: 48.3794, lng: 31.1656 },
     zoom: 6,
-  }
+  }, 
+  places: [],
 };
--- fe/src/layouts/map-layout/map-layout.jsx
+++ fe/src/layouts/map-layout/map-layout.jsx
@@ -2,32 +2,11 @@ import React from 'react';
 import { GoogleMap } from '../../components';
 import './map-layout.css';
 
-const MapLayout = () => {
-
-  const addMarkers = (map, places) => {
-    places.forEach((place, index) => {
-      const marker = new window.google.maps.Marker({
-        map,
-        position: place.coords,
-        label: `${index + 1}`,
-        title: place.title,
-      })
-      marker.addListener(`click`, () => {
-        window.location.href = place.url
-      })
-    })
-  };
- 
-  const places = [{
-    coords: { lat: 49.9935, lng: 36.2304 }, // required: latitude & longitude at which to display the marker
-    title: `Mashrooms here`, // optional
-    url: `https://en.wikipedia.org/wiki/Amanita`, // optional
-  }]
-
+const MapLayout = () => { 
   return (
   <div className="side left-side">
     <div className="side__inner">
-      <GoogleMap onMount={addMarkers} places={places} />
+      <GoogleMap/>
     </div>
   </div>
   )
