62543b83d7979e8a5f4c5602c6c327d878ed9467
Master0fMagic <daniil.tovstolug@gmail.com>
2020-05-16 17:13:34 +0300

    route chenged

--- be/routes/geopoint.route.js
+++ be/routes/geopoint.route.js
@@ -2,7 +2,7 @@ const express = require('express');
 const geopoint = require('../controllers/geopoint.controller');
 const router = express.Router();
 
-router.post('/geopoint', (req, res) => {
+router.post('/geopoint/save', (req, res) => {
     geopoint.create(req, res);
 })
 
