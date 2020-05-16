bd197cee9681906a2a983d75486ca2fe502cd3fc
Master0fMagic <daniil.tovstolug@gmail.com>
2020-05-16 16:28:19 +0300

    add try catch

--- be/utils/fileParser.js
+++ be/utils/fileParser.js
@@ -9,56 +9,81 @@ var parseExcelFile = (filename) => {
     //open file
     //adressees - array with adresses
     var adresses = [];
-    var wb = xlsx.readFile(filename);
-    //read 1st sheet
-    var worksheet = wb.Sheets[wb.SheetNames[0]];
-    //convert data to json
-    //data - array of json objects
-    let data = xlsx.utils.sheet_to_json(worksheet);
+    try {
+        var wb = xlsx.readFile(filename);
+        //read 1st sheet
+        var worksheet = wb.Sheets[wb.SheetNames[0]];
+        //convert data to json
+        //data - array of json objects
+        let data = xlsx.utils.sheet_to_json(worksheet);
+    }
+    catch (ex) {
+        return adresses;
+    }
     //making string with adress
-    data.forEach(function (val, i, data) {
-        if (i != 0) {
-            var adress = val.область + '  ' + val.область_тип + ' ';
-            if (val.район != null) adress += val.район + ' ' + val.район_тип;
-            if (val['назва населеного пункту'] != null) adress += ' ' + val['назва населеного пункту'];
-            adresses.push(adress);
-        }
+    try {
+        data.forEach(function (val, i, data) {
+            if (i != 0) {
+                var adress = val.область + '  ' + val.область_тип + ' ';
+                if (val.район != null) adress += val.район + ' ' + val.район_тип;
+                if (val['назва населеного пункту'] != null) adress += ' ' + val['назва населеного пункту'];
+                adresses.push(adress);
+            }
+        });
+    }
+    catch (ex) {
+        
+    }
 
-    });
     return adresses;
 };
 var parseCsvFile = (filename) => {
     //results - array of json objects
+
     var adresses = [];
     //reading csv file
-    fs.createReadStream(filename)
-        .pipe(csv())
-        .on('data', (data) => adresses.push(data.adress))
-        .on('end', () => { });
+    try {
+        fs.createReadStream(filename)
+            .pipe(csv())
+            .on('data', (data) => adresses.push(data.adress))
+            .on('end', () => { });
+    }
+    catch (ex) {
+        console.log(">>>Error while reeading csv file " + ex);
+    }
     return adresses;
 };
 var formingBatch = (adresses) => {
     let batch = [];   //array with 10 adresses
-    var ads = [];     //array with n batches
-    for(let i=0;i<adresses.length;i++)
-    {
-        if (((i % 10) == 9) && (i != 0)) {
-            batch.push(adresses[i]);
-            ads.push(batch);
-            batch = [];
+    var ads = [];
+    try {     //array with n batches
+        for (let i = 0; i < adresses.length; i++) {
+            if (((i % 10) == 9) && (i != 0)) {
+                batch.push(adresses[i]);
+                ads.push(batch);
+                batch = [];
+            }
+            else if (i == (adresses.length - 1)) {
+                batch.push(adresses[i]);
+                ads.push(batch);
+            }
+            else batch.push(adresses[i]);
         }
-        else if (i == (adresses.length - 1)) {
-            batch.push(adresses[i]);
-            ads.push(batch);
-        }
-        else batch.push(adresses[i]);
+    }
+    catch (ex) {
+
     }
     return ads;
 };
 
 exports.parse = (filename) => {
-    if (path.extname(filename) == '.xls' || path.extname(filename) == '.xlsx')
-        return parseExcelFile(filename);
-    else if (path.extname(filename) == '.csv')
-        return parseCsvFile(filename);
+    try {
+        if (path.extname(filename) == '.xls' || path.extname(filename) == '.xlsx')
+            return parseExcelFile(filename);
+        else if (path.extname(filename) == '.csv')
+            return parseCsvFile(filename);
+    }
+    catch (ex) {
+        return [];
+    }
 };
